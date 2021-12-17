0e476028b3700ca3507c2be76f843950dcfc5c8d
Lowella Fox <foxlo25@c02dqecjmd6m.uswin.ad.vzwcorp.com>
2021-12-17 15:15:25 -0600

    Deleted the alert messages

--- handlebars/starting-point/public/updateRestaurants.js
+++ handlebars/starting-point/public/updateRestaurants.js
@@ -1,10 +1,6 @@
 async function updateRestaurants(restaurantId){
-    alert(restaurantId)
     let name = document.querySelector("#name").value
     let image = document.querySelector("#image").value
-
-    alert(name)
-    alert(image)
 
     let res = await fetch(`/update-restaurants/${restaurantId}`, {
         method: 'PUT',
